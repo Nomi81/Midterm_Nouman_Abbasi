fac2999fe3be51b01ce3ed8e2f22c59263c7ae25
Nomi81 <38571167+Nomi81@users.noreply.github.com>
2024-01-29 19:32:31 +0500

    Add files via upload

---
+++ Midterm-1.py
@@ -0,0 +1,53 @@
+#question number 1
+# int=num1 =float(input("enter any number"))
+# int=num2 =float(input("enter any number"))
+# int =num3=num1+num2
+# print(num3)
+
+#Question Number 2
+# a="Hello"
+# print(a)
+# a=input("Enter Any Name ")
+# print(a)
+
+#question Number 4
+# dis=["orange","mango","Apple"]
+# print(dis)
+# dis.append(input('save other fruit name'))
+# print(dis)
+
+# Question Number 5
+# price = 12.23
+# conver = int(price)
+# print("Orignal Value",price)
+# print("Converted Value",conver)
+# print(type(conver))
+
+# Question Number 6
+# Student={
+#     "Name": "Nouman",
+#     "Age": "24",
+#     "grade": "B",
+    
+# }
+# print(Student)
+
+
+# Question Number 7
+# int = a =float( input("Enter Your Age"))
+# if a >= 18 :
+
+#     print("You Are 18+")
+# else:
+#     print("You Are Teenager")
+
+# Question number 9
+# a="Good "
+# b="Morning"
+# c=a+b
+# print(c)
+
+# Question Number 10
+# Days_of_week = ("Monday","tuesday","wednesday","thursday","friday","saturday","sunday")
+# print(Days_of_week)
+# print(Days_of_week[2])
